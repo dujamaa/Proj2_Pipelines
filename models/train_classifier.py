8bf04933ab7ef1bd21350ea40791a0860d6cf6b4
Dawud <dujamaa@yahoo.com>
2020-05-23 21:59:42 -0400

    feat: Refactor ML Pipeline
    Begin refactor of the ML Pipeline Notebook
    to the train_classifier.py file.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,121 @@
+# import libraries
 import sys
+import re
+import pickle
+import numpy as np
+import pandas as pd
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+from sqlalchemy import create_engine
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def load_data(database_filepath):
-    pass
+    """Load data from SQLite database.
+        Args: 
+            database_filepath: SQLite database file path and name                    
+        Returns: 
+            X: the column of messages used to predict categories
+            Y: 36 category features to predict based on messages
+            category_names: names of the 36 category features
+    """
+    dialectobject = 'sqlite:///'
+    do_db = dialectobject + database_filepath
+    engine = create_engine(do_db)
+    df = pd.read_sql("SELECT * FROM MessageCats", engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """Normalize, clean, lemmatize, and tokenize text
+        Args: 
+            text: text to be normalized, cleaned, and tokenized                   
+        Returns: 
+            clean_tokens: cleaned and tokenized text
+    """
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+   
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:        
+        # lemmatize and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """Machine Learning pipeline model
+        Args: 
+            None                   
+        Returns: 
+            model
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """ Report f1 score, precision and recall for each category
+        Args: 
+            model: the NLP/MLP Pipeline model 
+            X_test: test split of the messages
+            Y_test: test split of the categories
+            category_names: names of the 36 category features
+        Returns: 
+            None
+    """
+    Y_pred = model.predict(X_test)
+    Y_pred2 = pd.DataFrame(Y_pred, columns = category_names)
+    
+    for category in category_names:
+        categoryDF = pd.DataFrame(
+            classification_report(Y_test[category], Y_pred2[category], zero_division=0,output_dict=True)
+        )
+        print(category)
+        print('    Accuracy: {}%  Precision: {}%  Recall: {}%\n'.format(
+            categoryDF.loc['f1-score','weighted avg'].round(4),
+            categoryDF.loc['precision','weighted avg'].round(4),
+            categoryDF.loc['recall','weighted avg'].round(4))
+             )
 
 
 def save_model(model, model_filepath):
-    pass
+    """ Export model as a pickle file        
+        Args: 
+            model: the model to be exported 
+            model_filepath: the file name and path to expor the model
+        Returns: 
+            None
+    """    
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
