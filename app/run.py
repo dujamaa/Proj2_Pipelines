9f0c8d45c083c7ea4a48bb52ada405c71f2f839d
Dawud <dujamaa@yahoo.com>
2020-05-25 14:19:01 -0400

    feat: Add 2 visuals to the run.py file
    Add 2 visualizations on number of messages with no category by
    genre, and number of messages per category.

--- app/run.py
+++ app/run.py
@@ -43,6 +43,28 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # Count the number of messages in each category
+    category_counts = df.astype(bool).sum(axis = 0)
+    category_counts.drop(['id','message','original','genre'], inplace = True)
+    category_names = list(category_counts.index)
+    
+    # Count the number of messages with no categoy by genre
+    no_category = df[(df.related == 0) & (df.request == 0) & (df.offer == 0) & 
+                 (df.aid_related == 0) & (df.medical_help == 0) & (df.medical_products == 0) & 
+                 (df.search_and_rescue == 0) & (df.security == 0) & (df.military == 0) & 
+                 (df.child_alone == 0) & (df.water == 0) & (df.food == 0) & (df.shelter == 0) & 
+                 (df.clothing == 0) & (df.money == 0) & (df.missing_people == 0) & 
+                 (df.refugees == 0) & (df.death == 0) & (df.other_aid == 0) & 
+                 (df.infrastructure_related == 0) & (df.transport == 0) & 
+                 (df.buildings == 0) & (df.electricity == 0) & (df.tools == 0) & 
+                 (df.hospitals == 0) & (df.shops == 0) & (df.aid_centers == 0) & 
+                 (df.other_infrastructure == 0) & (df.weather_related == 0) & 
+                 (df.floods == 0) & (df.storm == 0) & (df.fire == 0) & (df.earthquake == 0) & 
+                 (df.cold == 0) & (df.other_weather == 0) & (df.direct_report == 0) 
+                ]
+    no_category_count = no_category.groupby('genre').count()['message']
+    no_ctgry_genre_names = list(no_category_count.index)
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,6 +85,40 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data':[
+                Bar(
+                    x=no_ctgry_genre_names,
+                    y=no_category_count
+                )
+            ],
+            'layout':{
+                'title': 'Distribution of Message Genres with no Categorization',
+                'yaxis':{
+                    'title':"Count"
+                },
+                'xaxis':{
+                    'title':"Genre"
+                }
+            }    
+        },
+        {
+            'data':[
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+            'layout':{
+                'title': 'Distribution of Messages per Category',
+                'yaxis':{
+                    'title':"Count"
+                },
+                'xaxis':{
+                    'title':"Category"
+                }
+            }    
         }
     ]
     
